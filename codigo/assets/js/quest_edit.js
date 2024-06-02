0f7ed4c1e33160fa0fca9dacd7e36fea1dd6f902
Miguel-Ol <miguel.faria@sga.pucminas.br>
2024-06-01 23:20:22 -0300

    adição do aqruivo json

--- codigo/assets/js/quest_edit.js
+++ codigo/assets/js/quest_edit.js
@@ -6,6 +6,7 @@ function leDados() {
     objDados = JSON.parse(strDados);
   }
   else {
+    //formato no qual estou armazenando as questões
     objDados = {
       dificuldades: [
         {
@@ -13,8 +14,8 @@ function leDados() {
             {
               id: 1,
               texto: "qual destes tipos de variável aceita um texto?",
-              resp: ["string", "byte", "int", "double"],
-              vouf: ["true","false","false","false"]
+              resp: ["string", "byte", "int", "double"]//,
+              //vouf: ["true","false","false","false"]
             }
           ]
         }
@@ -25,6 +26,7 @@ function leDados() {
   return objDados;
 }
 
+//funções de armazenar o texto no local storage
 function incluirQuest() {
   let objDados = leDados();
 
@@ -51,44 +53,42 @@ function salvaDados(dados) {
   console.log(localStorage.getItem('db'));
 }
 
-
 function imprimeDados() {
   let tela = document.getElementById('EspQuests');
   let strHtml = '';
   let objDados = leDados();
 
+  //após uma questão ser confirmada a criação, esse será seu escopo
   for (let i = 0; i < objDados.dificuldades[0].fácil.length; i++) {
-    if (objDados.dificuldades && objDados.dificuldades[0] && objDados.dificuldades[0].fácil && objDados.dificuldades[0].fácil[i] && objDados.dificuldades[0].fácil[i].resp && objDados.dificuldades[0].fácil[i].resp.length > 3) {
       strHtml += `<div class="questão${[i]} border-bottom mt-2">
-        <div class="d-flex col">
-            <button id="BtnDelQuest${[i]}"><img src="../assets/images/trash_red.png" class="" alt="" width="35"
-                    height="auto"></button>
-            <div class="fs-3 ms-2 d-flex justify-content-between">
-                <h2 class="mt-1">${objDados.dificuldades[0].fácil[i].texto}</h2>
-                <button id="BtnEditarPerg${[i]}" class="ms-2"><img src="../assets/images/iconizer-botao-editar.svg" class=""
-                        alt="" width="40" height="40"></button>
-            </div>
-        </div>
-        <ul>
-            <li class="d-flex"><button><img src="../assets/images/iconizer-botao-editar.svg"
-                        class="editarResp me-2 mb-3"id="BtnEditarResp1${[i]}" alt=""></button>
-                <p>${objDados.dificuldades[0].fácil[i].resp[0]}</p>
-            </li>
-            <li class="d-flex"><button><img src="../assets/images/iconizer-botao-editar.svg"
-                        class="editarResp me-2 mb-3"id="BtnEditarResp2${[i]}" alt=""></button>
-                <p>${objDados.dificuldades[0].fácil[i].resp[1]}</p>
-            </li>
-            <li class="d-flex"><button><img src="../assets/images/iconizer-botao-editar.svg"
-                        class="editarResp me-2 mb-3"id="BtnEditarResp3${[i]}" alt=""></button>
-                <p>${objDados.dificuldades[0].fácil[i].resp[2]}</p>
-            </li>
-            <li class="d-flex"><button><img src="../assets/images/iconizer-botao-editar.svg"
-                        class="editarResp me-2 mb-3"id="BtnEditarResp4${[i]}" alt=""></button>
-                <p>${objDados.dificuldades[0].fácil[i].resp[3]}<p>
-            </li>
-        </ul>
-    </div>`
-    }
+      <div class="d-flex col">
+          <button id="BtnDelQuest${[i]}"><img src="../assets/images/trash_red.png" class="" alt="" width="35"
+                  height="auto"></button>
+          <div class="fs-3 ms-2 d-flex justify-content-between">
+              <h2 class="mt-1">${objDados.dificuldades[0].fácil[i].texto}</h2>
+              <button id="BtnEditarPerg${[i]}" class="ms-2"><img src="../assets/images/iconizer-botao-editar.svg" class=""
+                      alt="" width="40" height="40"></button>
+          </div>
+      </div>
+      <ul>
+          <li class="d-flex"><button id="editrespind1${i}"><img src="../assets/images/circle2.png"
+                      class="editarResp me-2 mb-3"id="BtnEditarResp1${[i]}" alt=""></button>
+              <p id="respp1${i}">${objDados.dificuldades[0].fácil[i].resp[0]}</p>
+          </li>
+          <li class="d-flex"><button id="editrespind2${i}"><img src="../assets/images/circle2.png"
+                      class="editarResp me-2 mb-3"id="BtnEditarResp2${[i]}" alt=""></button>
+              <p id="respp2${i}">${objDados.dificuldades[0].fácil[i].resp[1]}</p>
+          </li>
+          <li class="d-flex"><button id="editrespind3${i}"><img src="../assets/images/circle2.png"
+                      class="editarResp me-2 mb-3"id="BtnEditarResp3${[i]}" alt=""></button>
+              <p id="respp3${i}">${objDados.dificuldades[0].fácil[i].resp[2]}</p>
+          </li>
+          <li class="d-flex"><button id="editrespind4${i}"><img src="../assets/images/circle2.png"
+                      class="editarResp me-2 mb-3"id="BtnEditarResp4${[i]}" alt=""></button>
+              <p id="respp4${i}">${objDados.dificuldades[0].fácil[i].resp[3]}<p>
+          </li>
+      </ul>
+  </div>`
   }
   tela.innerHTML = strHtml;
   document.getElementById('InputPerg').value = '';
@@ -97,6 +97,7 @@ function imprimeDados() {
   document.getElementById('InputQuest3').value = '';
   document.getElementById('InputQuest4').value = '';
 
+//comando de deletar questões
   for (let i = 0; i < objDados.dificuldades[0].fácil.length; i++) {
     document.getElementById(`BtnDelQuest${i}`).addEventListener('click', function() {
         let questao = document.querySelector(`.questão${i}`);
@@ -108,7 +109,41 @@ function imprimeDados() {
 
         location.reload();
     });
-}
+
+    //comando de edição de questão
+document.getElementById(`BtnEditarPerg${[i]}`).addEventListener('click', function() {
+  let questao = document.querySelector(`.questão${i}`);
+  let div = document.getElementById("TelaAddQuest");
+  let addbtn = document.getElementById("BtnAddQuestArea");
+  let inc = document.getElementById("cancelarea");
+
+  let texto = objDados.dificuldades[0].fácil[i].texto;
+  let resp0 = objDados.dificuldades[0].fácil[i].resp[0];
+  let resp1 = objDados.dificuldades[0].fácil[i].resp[1];
+  let resp2 = objDados.dificuldades[0].fácil[i].resp[2];
+  let resp3 = objDados.dificuldades[0].fácil[i].resp[3];
+
+  questao.remove();
+  objDados.dificuldades[0].fácil.splice(i, 1);
+
+  if (div.style.display === "none") {
+    div.style.display = "block";
+    addbtn.style.display = "none";
+  }
+  if(inc.style.display === "block")
+  inc.style.display = "none";
+
+  document.getElementById('InputPerg').value = texto;
+  document.getElementById('InputQuest1').value = resp0;
+  document.getElementById('InputQuest2').value = resp1;
+  document.getElementById('InputQuest3').value = resp2;
+  document.getElementById('InputQuest4').value = resp3;
+  salvaDados(objDados);
+});
+  
+    }
+    
+    
 }
 
 
@@ -120,20 +155,24 @@ document.getElementById('BtnConfQuest').addEventListener('click', incluirQuest);
 
 
 
-
+//comando para a aparição do campo de cadastro de questão
 document.getElementById("BtnAddQuest").addEventListener("click", function () {
   var div = document.getElementById("TelaAddQuest");
   var addbtn = document.getElementById("BtnAddQuestArea");
+  var cancbtn = document.getElementById("cancelarea");
 
   if (div.style.display === "none") {
     div.style.display = "block";
     addbtn.style.display = "none";
+    cancbtn.style.display = "block";
   } else {
     div.style.display = "none";
     addbtn.style.display = "block";
+    cancbtn.style.display = "none";
   }
 });
 
+//comando de confirmar a criação de uma questão
 document.getElementById("BtnConfQuest").addEventListener("click", function () {
   var div = document.getElementById("TelaAddQuest");
   var addbtn = document.getElementById("BtnAddQuestArea");
@@ -146,6 +185,7 @@ document.getElementById("BtnConfQuest").addEventListener("click", function () {
   }
 });
 
+//comando de cancelar a criação de uma questão
 document.getElementById("BtnCancelQuest").addEventListener("click", function () {
   var div = document.getElementById("TelaAddQuest");
   var addbtn = document.getElementById("BtnAddQuestArea");
@@ -160,8 +200,17 @@ document.getElementById("BtnCancelQuest").addEventListener("click", function () 
     document.getElementById('InputQuest2').value = '';
     document.getElementById('InputQuest3').value = '';
     document.getElementById('InputQuest4').value = '';
-
   }
 });
+
+/*document.getElementById("editrespind1").addEventListener("click", function () {
+  var delp = document.getElementById("respp1");
+  delp.remove();
+  let altht = '';
+  altht = `<input type="text"
+  class="form-control" aria-label="Sizing example input"
+  aria-describedby="inputGroup-sizing-sm">`
+  this.innerHTML = altht;
+});*/
 
 
